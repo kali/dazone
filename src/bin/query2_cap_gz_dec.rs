85c1dc3d10d83fb9976648c382f7246708c01c7e
Mathieu Poumeyrol <kali@zoy.org>
2016-01-07 17:52:23 +0100

    working multi map

--- src/bin/query2_cap_gz_dec.rs
+++ src/bin/query2_cap_gz_dec.rs
@@ -1,3 +1,4 @@
+#![feature(iter_arith)]
 extern crate dx16;
 extern crate glob;
 extern crate capnp;
@@ -24,31 +25,55 @@ fn main() {
                                .long("length")
                                .help("length (8, 10, or 12)")
                                .takes_value(true))
+                      .arg(Arg::with_name("REDUCE")
+                               .short("r")
+                               .long("reduce")
+                               .help("reduce strategy: mono multi")
+                               .takes_value(true))
+                      .arg(Arg::with_name("REDUCE PAR")
+                               .short("p")
+                               .long("parallelism")
+                               .help("multi reduce par factor")
+                               .takes_value(true))
                       .get_matches();
     let set = matches.value_of("SET").unwrap_or("5nodes");
     let length: usize = matches.value_of("LENGTH").unwrap_or("8").parse().unwrap();
-    scan(set, length).unwrap();
+    let par: usize = matches.value_of("REDUCE PAR").unwrap_or("16").parse().unwrap();
+    match matches.value_of("REDUCE").unwrap_or("multi") {
+        "multi" => multi(set, length, par).unwrap(),
+        "mono" => mono(set, length).unwrap(),
+        s => panic!("unknown strategy {}", s),
+    }
 }
 
-fn scan(set: &str, length: usize) -> Dx16Result<()> {
+fn mapper(length: usize, reader: Dx16Result<Reader<OwnedSegments>>) -> Emit<[u8; 12], f32> {
+    let reader = reader.unwrap();
+    let visit: ::dx16::cap::user_visits::Reader = reader.get_root()
+                                                        .unwrap();
+    let mut chars = [b' '; 12];
+    for (i, v) in visit.get_source_i_p()
+                       .unwrap()
+                       .bytes()
+                       .take(::std::cmp::min(chars.len(), length))
+                       .enumerate() {
+        chars[i] = v;
+    }
+    Emit::One(chars, visit.get_ad_revenue())
+}
+
+fn mono(set: &str, length: usize) -> Dx16Result<()> {
     let bibi = dx16::bibi_cap_gz_dec(set, "uservisits");
-    let result = map_reduce(|reader: Dx16Result<Reader<OwnedSegments>>| {
-                                let reader = reader.unwrap();
-                                let visit: ::dx16::cap::user_visits::Reader = reader.get_root()
-                                                                                    .unwrap();
-                                let mut chars = [b' '; 12];
-                                for (i, v) in visit.get_source_i_p()
-                                                   .unwrap()
-                                                   .bytes()
-                                                   .take(::std::cmp::min(chars.len(), length))
-                                                   .enumerate() {
-                                    chars[i] = v;
-                                }
-                                Emit::One(chars, visit.get_ad_revenue())
-                            },
-                            |a, b| a + b,
-                            bibi);
+    let result = map_reduce(|r| mapper(length, r), |a, b| a + b, bibi);
     println!("{} groups", result.len());
+
+    Ok(())
+}
+
+fn multi(set: &str, length: usize, par: usize) -> Dx16Result<()> {
+    let bibi = dx16::bibi_cap_gz_dec(set, "uservisits");
+    let result = map_par_reduce(|r| mapper(length, r), |a, b| a + b, par, bibi);
+    let len: usize = result.iter().map(|h| h.len()).sum();
+    println!("{} groups", len);
 
     Ok(())
 }
--- src/mapred.rs
+++ src/mapred.rs
@@ -15,22 +15,38 @@ pub enum Emit<K, V> {
     Vec(Vec<(K, V)>),
 }
 
-trait Aggregator<R,K,V>
+// ************************** AGGREGATOR / INLET *****************************
+
+pub trait Aggregator<R,K,V>
     where R: Sync + Fn(&V, &V) -> V + 'static,
         K: Send + Eq + ::std::hash::Hash + 'static,
         V: Send + 'static
 {
     fn create_inlet<'b>(&'b self) -> Box<Inlet<R, K, V> + 'b>;
+    fn converge(&mut self) {}
 }
 
-
-trait Inlet<R, K, V>
+pub trait Inlet<R, K, V>
     where R: Sync + Fn(&V, &V) -> V + 'static,
           K: Send + Eq + ::std::hash::Hash + 'static,
           V: Send + 'static
 {
-    fn push(&mut self, e: Emit<K, V>);
+    fn push_one(&mut self, k: K, v: V);
+
+    fn push(&mut self, e: Emit<K, V>) {
+        match e {
+            Emit::None => (),
+            Emit::One(k, v) => self.push_one(k, v),
+            Emit::Vec(mut v) => {
+                for p in v.drain(..) {
+                    self.push_one(p.0, p.1)
+                }
+            }
+        }
+    }
 }
+
+// ************************** HashMap Aggregator *****************************
 
 struct HashMapAggregator<'a, R, K, V>
     where R: Sync + Fn(&V, &V) -> V + 'static,
@@ -70,16 +86,8 @@ impl<'a, 'b, R, K, V> Inlet<R, K, V> for HashMapInlet<'a, 'b, R, K, V>
           V: Send + 'static,
           'a: 'b
 {
-    fn push(&mut self, e: Emit<K, V>) {
-        match e {
-            Emit::None => (),
-            Emit::One(k, v) => update_hashmap(&mut self.partial, self.parent.reducer, k, v),
-            Emit::Vec(mut v) => {
-                for p in v.drain(..) {
-                    update_hashmap(&mut self.partial, self.parent.reducer, p.0, p.1)
-                }
-            }
-        }
+    fn push_one(&mut self, k: K, v: V) {
+        update_hashmap(&mut self.partial, self.parent.reducer, k, v);
     }
 }
 
@@ -92,6 +100,86 @@ impl<'a, 'b, R, K, V> Drop for HashMapInlet<'a, 'b, R, K, V>
         let mut locked = self.parent.hashmap.lock().unwrap();
         for (k, v) in self.partial.drain() {
             update_hashmap(&mut locked, self.parent.reducer, k, v);
+        }
+    }
+}
+
+// ************************** MultiHashMap Aggregator *************************
+
+struct MultiHashMapAggregator<'a, R, K, V>
+    where R: Sync + Fn(&V, &V) -> V + 'static,
+          K: Send + Eq + ::std::hash::Hash + 'static,
+          V: Send + 'static
+{
+    hashmaps: Vec<Mutex<&'a mut HashMap<K, V>>>,
+    reducer: &'a R,
+}
+
+impl<'a, R, K, V> MultiHashMapAggregator<'a, R, K, V>
+    where R: Sync + Fn(&V, &V) -> V + 'static,
+          K: Send + Eq + ::std::hash::Hash + 'static,
+          V: Send + 'static
+{
+    fn partition(&self, k: &K) -> usize {
+        use std::hash::{Hasher, SipHasher};
+        let mut s = SipHasher::new();
+        k.hash(&mut s);
+        s.finish() as usize % self.hashmaps.len()
+    }
+}
+
+
+impl<'a, R, K, V> Aggregator<R, K, V> for MultiHashMapAggregator<'a, R, K, V>
+    where R: Sync + Fn(&V, &V) -> V + 'static,
+          K: Send + Eq + ::std::hash::Hash + 'static,
+          V: Send + 'static
+{
+    fn create_inlet<'b>(&'b self) -> Box<Inlet<R, K, V> + 'b> {
+        let mut v = Vec::with_capacity(self.hashmaps.len());
+        for _ in 0..self.hashmaps.len() {
+            v.push(HashMap::new())
+        }
+        Box::new(MultiHashMapInlet {
+            parent: &self,
+            hashmaps: v,
+        })
+    }
+}
+
+struct MultiHashMapInlet<'a, 'b, R, K, V>
+    where R: Sync + Fn(&V, &V) -> V + 'static,
+          K: Send + Eq + ::std::hash::Hash + 'static,
+          V: Send + 'static,
+          'a: 'b
+{
+    parent: &'b MultiHashMapAggregator<'a, R, K, V>,
+    hashmaps: Vec<HashMap<K, V>>,
+}
+
+impl<'a, 'b, R, K, V> Inlet<R, K, V> for MultiHashMapInlet<'a, 'b, R, K, V>
+    where R: Sync + Fn(&V, &V) -> V + 'static,
+          K: Send + Eq + ::std::hash::Hash + 'static,
+          V: Send + 'static,
+          'a: 'b
+{
+    fn push_one(&mut self, k: K, v: V) {
+        let partial: usize = self.parent.partition(&k);
+        update_hashmap(&mut self.hashmaps[partial], self.parent.reducer, k, v)
+    }
+}
+
+impl<'a, 'b, R, V, K> Drop for MultiHashMapInlet<'a, 'b, R, K, V>
+    where R: Sync + Fn(&V, &V) -> V + 'static,
+          K: Send + Eq + ::std::hash::Hash + 'static,
+          V: Send + 'static,
+          'a: 'b
+{
+    fn drop(&mut self) {
+        for (total, mut partial) in self.parent.hashmaps.iter().zip(self.hashmaps.drain(..)) {
+            let mut locked = total.lock().unwrap();
+            for (k, v) in partial.drain() {
+                update_hashmap(&mut locked, self.parent.reducer, k, v);
+            }
         }
     }
 }
@@ -113,41 +201,34 @@ fn update_hashmap<'h, 'r, R, K, V>(hash: &'h mut HashMap<K, V>, reducer: &'r R, 
     }
 }
 
-pub struct MapReduceOp<'a, M, R, A, K, V>
+pub struct MapOp<'a, M, A, K, V>
     where M: Sync + Fn(A) -> Emit<K, V>,
-          R: Sync + Fn(&V, &V) -> V + 'static,
           A: Send,
           K: Send + Eq + ::std::hash::Hash + 'static,
           V: Send + 'static
 {
     mapper: M,
-    reducer: R,
     _phantom: ::std::marker::PhantomData<A>,
     _phantom_2: ::std::marker::PhantomData<&'a usize>,
 }
 
-impl<'a, M, R, A, K, V> MapReduceOp<'a, M, R, A, K, V>
+impl<'a, M, A, K, V> MapOp<'a, M, A, K, V>
     where M: Sync + Fn(A) -> Emit<K, V>,
-          R: Sync + Fn(&V, &V) -> V + 'static,
           A: Send,
           K: Send + Eq + ::std::hash::Hash + 'static,
           V: Send + 'static
 {
-    pub fn run(&self, chunks: BI<BI<A>>) -> HashMap<K, V> {
-        let reducer = &self.reducer;
+    pub fn run<Agg, R>(&self, chunks: BI<BI<A>>, aggregator: &Agg)
+        where Agg: Aggregator<R, K, V> + Sync,
+              R: Sync + Fn(&V, &V) -> V + 'static
+    {
         let mapper = &self.mapper;
         println!("Mapping...");
         let mut rawpb = ProgressBar::new(chunks.size_hint().0);
         rawpb.format(" _🐌·🍀");
         let pb = Mutex::new(rawpb);
 
-        let mut result: HashMap<K, V> = HashMap::new();
         {
-            let aggregator = HashMapAggregator {
-                hashmap: Mutex::new(&mut result),
-                reducer: reducer,
-            };
-
             let each = |it: BI<A>| {
                 {
                     let mut inlet = aggregator.create_inlet();
@@ -163,13 +244,11 @@ impl<'a, M, R, A, K, V> MapReduceOp<'a, M, R, A, K, V>
             }
 
         }
-        result
     }
 
-    pub fn new_map_reduce(map: M, reduce: R) -> MapReduceOp<'a, M, R, A, K, V> {
-        MapReduceOp {
+    pub fn new_map_reduce(map: M) -> MapOp<'a, M, A, K, V> {
+        MapOp {
             mapper: map,
-            reducer: reduce,
             _phantom: ::std::marker::PhantomData,
             _phantom_2: ::std::marker::PhantomData,
         }
@@ -183,7 +262,39 @@ pub fn map_reduce<'a, M, R, A, K, V>(map: M, reduce: R, chunks: BI<BI<A>>) -> Ha
           K: Send + Eq + ::std::hash::Hash + 'static,
           V: Send + 'static
 {
-    MapReduceOp::new_map_reduce(map, reduce).run(chunks)
+    let mut result: HashMap<K, V> = HashMap::new();
+    {
+        let mut aggregator = HashMapAggregator {
+            hashmap: Mutex::new(&mut result),
+            reducer: &reduce,
+        };
+        MapOp::new_map_reduce(map).run(chunks, &aggregator);
+        aggregator.converge()
+    }
+    result
+}
+
+pub fn map_par_reduce<'a, M, R, A, K, V>(map: M,
+                                         reduce: R,
+                                         partitions: usize,
+                                         chunks: BI<BI<A>>)
+                                         -> Vec<HashMap<K, V>>
+    where M: Sync + Fn(A) -> Emit<K, V>,
+          R: Sync + Fn(&V, &V) -> V + 'static,
+          A: Send,
+          K: Send + Eq + ::std::hash::Hash + 'static,
+          V: Send + 'static
+{
+    let mut result: Vec<HashMap<K, V>> = (0..partitions).map(|_| HashMap::new()).collect();
+    {
+        let mut aggregator = MultiHashMapAggregator {
+            hashmaps: result.iter_mut().map(|h| Mutex::new(h)).collect(),
+            reducer: &reduce,
+        };
+        MapOp::new_map_reduce(map).run(chunks, &aggregator);
+        aggregator.converge()
+    }
+    result
 }
 
 
