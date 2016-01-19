44a75d5214c819fa940b88199f4e77581ba14e1d
Mathieu Poumeyrol <kali@zoy.org>
2016-01-19 16:29:15 +0100

    slightly nicer query2_simple formatting

--- src/bin/query2_simple.rs
+++ src/bin/query2_simple.rs
@@ -1,14 +1,15 @@
 extern crate dazone;
 
 use dazone::crunch::*;
+use dazone::files::bibi_pod;
 use dazone::short_bytes_array::*;
 use dazone::data::pod::UserVisits;
 
 fn main() {
-    let input = dazone::files::bibi_pod::<UserVisits>("5nodes", "uservisits", "text-deflate");
+    let input = bibi_pod("5nodes", "uservisits", "text-deflate");
     let map = |visit: UserVisits| Emit::One(K8::prefix(&*visit.source_ip), visit.ad_revenue);
     let reduce = |a: &f32, b: &f32| a + b;
-    let mut aggregator = ::dazone::crunch::aggregators::MultiHashMapAggregator::new(&reduce, 256);
+    let mut aggregator = aggregators::MultiHashMapAggregator::new(&reduce, 256);
     MapOp::new_map_reduce(map)
         .with_progress(true)
         .with_workers(16)
